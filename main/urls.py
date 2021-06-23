a082b193c4b67dda28bd54cae72ea6c916d2c5d1
Liottt <0553615512a@gmail.com>
2021-06-23 21:59:15 +0600

    add authead

--- main/urls.py
+++ main/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls'))
+    path('shop/', include('shop.urls')),
+    path('', include('authead.urls')),
 ]
