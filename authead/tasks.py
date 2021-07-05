9ce4357e8675a4b0dffad1786750a8eb51a6f15c
Liottt <80908141+Liottt@users.noreply.github.com>
2021-07-05 21:27:42 +0600

    User_expired

--- authead/tasks.py
+++ authead/tasks.py
@@ -1,7 +1,7 @@
 from main.celery import app
 from django.conf import settings
 from django.core.mail import send_mail
-from .models import ConfirmationCode
+from .models import ConfirmationCode, MainUser
 
 @app.task()
 def send_verify_url(text,email):
@@ -11,3 +11,10 @@ def send_verify_url(text,email):
 @app.task()
 def code_expired(code_id: int):
     ConfirmationCode.objects.get(id=code_id).delete()
+
+
+@app.task()
+def user_expired(user_id: int):
+    if MainUser.objects.active = False
+        MainUser.objects.get(id=User_id).delete()
+    
